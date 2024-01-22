67e5886a9e543f93df9a09d9387449c9fa01fd8f
Kavin Jayakumar <kavinjayakumar8@gmail.com>
2024-01-22 11:36:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-# Test
+# Imports
+import sys
+import time
+import random
+import tkinter
+import customtkinter
+import tkinter.messagebox
 
-def main():
-    pass
+# Main app class
+class App(customtkinter.CTk):
 
-if __name__ == "__main__":
-    main()
+    # Initialisation
+    def __init__(self):
+        super().__init__()
+        
+
+    # Class methods
+    ...
+
+
+# Commented for now
+
+# def main():
+#     pass
+
+# if __name__ == "__main__":
+#     main()
