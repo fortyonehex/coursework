184382503377eb07031244086307f4a2057c5cbf
Kavin Jayakumar <kavinjayakumar8@gmail.com>
2024-01-23 13:22:45 +0800

    u

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Imports
+import os
 import sys
 import time
 import random
@@ -6,52 +7,47 @@ import tkinter
 import pyrebase
 import customtkinter
 import tkinter.messagebox
+from dotenv import load_dotenv
 
 # Firebase configuration
-firebaseConfig = {
-  "apiKey": "AIzaSyAE6LtDc1r7UizS5LHnPiNwnVsiprUuPk0",
-  "authDomain": "coursework-e0974.firebaseapp.com",
-  'databaseURL': "https://coursework-e0974-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  "projectId": "coursework-e0974",
-  "storageBucket": "coursework-e0974.appspot.com",
-  "messagingSenderId": "726979196176",
-  "appId": "1:726979196176:web:93a505c63cf66090186195",
-  "measurementId": "G-BCJLPELCBE"
-}
+load_dotenv('.env')
 
-firebase = pyrebase.initialize_app(firebaseConfig)
+firebaseConfig = os.getenv('CONFIG')
 
-auth = firebase.auth()
+# firebase = pyrebase.initialize_app(firebaseConfig)
 
-while 1:
-    e = 'ok@ok.ok'
-    p = 'okokok'
-    s = 'si'
+# auth = firebase.auth()
 
-    if s == 'si':
-        try:
-            Login = auth.sign_in_with_email_and_password(e,p)
-            print('S')
-            print(Login)
-        except:
-            print('I')
-    else:
-        try:
-            Login = auth.create_user_with_email_and_password(e,p)
-            print('S')
-        except:
-            print('I')
+# e = 'ok@ok.ok'
+# p = 'okokok'
+# s = 'si'
 
-# Main app class
-class App(customtkinter.CTk):
+# if s == 'si':
+#     try:
+#         Login = auth.sign_in_with_email_and_password(e,p)
+#         print('S')
+#         print(Login)
+#         Login['displayName'] = 'Hello World!'
+#         print(Login)
+#     except:
+#         print('I')
+# else:
+#     try:
+#         Login = auth.create_user_with_email_and_password(e,p)
+#         print('S')
+#     except:
+#             print('I')
 
-    # Initialisation
-    def __init__(self):
-        super().__init__()
+# # Main app class
+# class App(customtkinter.CTk):
+
+#     # Initialisation
+#     def __init__(self):
+#         super().__init__()
         
 
-    # Class methods
-    ...
+#     # Class methods
+#     ...
 
 
 # Commented for now
